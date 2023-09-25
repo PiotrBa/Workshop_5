5a74fbb973d15d5b538e6b224d225769fa0a067a
PiotrBa <piotr.balazyk@wp.pl>
2023-09-25 17:00:41 +0100

    First commit

---
+++ src/main/java/org/example/services/BookService.java
@@ -0,0 +1,19 @@
+package org.example.services;
+
+import org.example.entity.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookService {
+    List<Book> getBooks();
+
+    Optional<Book> get(Long id);
+
+    void add(Book book);
+
+    void delete(Long id);
+
+    void update(Book book);
+}
+
